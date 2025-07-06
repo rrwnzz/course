da8406d5516e00979a2d1bcfc19250931c2eefed
rrwnzz <113715978+rrwnzz@users.noreply.github.com>
2025-07-06 13:17:33 +0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchAppBar from './component/nav';
+import Courses from './component/courses'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <SearchAppBar />
+      <Routes>
+        <Route path="/courses" element={<Courses />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
